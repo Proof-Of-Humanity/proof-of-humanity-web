f345ebdef5f881de06645faf03f6d00e8e8f1fe7
Henrique Barcelos <hbarcelos909@gmail.com>
2021-04-26 15:03:37 -0300

    chore: added toggle to disable SSR when building

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+const disableSSRMixin = {
+  exportTrailingSlash: true,
+  exportPathMap() {
+    return {
+      "/": { page: "/" },
+    };
+  },
+};
+
 module.exports = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
+  ...(process.env.DISABLE_SSR ? disableSSRMixin : {}),
 })({ target: "serverless" });
