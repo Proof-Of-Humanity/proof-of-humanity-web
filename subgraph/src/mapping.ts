1c0eec9b18a259e687235280a918d03ab32a5e3e
Matheus Faria de Alencar <malencar@pm.me>
2021-07-05 09:56:37 -0400

    fix: log warning if challenge was not found

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1424,7 +1424,10 @@ export function handleAppealPossible(event: AppealPossible): void {
       )
       .toHexString()
   );
-  if (challenge == null) return;
+  if (challenge == null) {
+    log.warning("Challenge not found, tx hash {}",[event.transaction.hash.toHexString()])
+    return;
+  }
 
   let arbitrator = KlerosLiquid.bind(event.address);
   let appealPeriodResult = arbitrator.appealPeriod(event.params._disputeID);
