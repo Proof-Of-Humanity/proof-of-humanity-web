7beedae93a7017804ffa2c923fb1b1fc77d8658b
Nico Bilinkis <nico@bilinkis.com>
2022-03-14 14:00:55 -0300

    show preview of ipfs media

--- _pages/profile/[id]/submit-profile/finalize-tab.js
+++ _pages/profile/[id]/submit-profile/finalize-tab.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Space } from 'antd';
+import {Row, Space, Col, Button} from 'antd';
 
 export default class FinalizeTab extends React.Component {
   constructor(props) {
@@ -7,18 +7,49 @@ export default class FinalizeTab extends React.Component {
     console.log('FinalizeTab props=', props);
   }
 
+
   render() {
+    console.log(this.props)
     // img, video and submitter name source by props
     return (
       <>
         <Row>
-          <Space direction='vertical' size={1}>
+          <Space direction='vertical'
+            size={1}>
             <h2>Finalize your registration</h2>
             <p>Verify your submission information and media is correct and submit the transaction to register</p>
-            <img></img>
-            <video></video>
-            {/* Next steps... */}
-          </Space>
+            <Col>
+              <div style={
+                {textAlign: "center"}
+              }>
+                This is your picture:
+                <img style={
+                    {width: "50%"}
+                  }
+                  src={
+                    this.props.state.imageURI
+                }></img>
+              </div>
+            </Col>
+            <Col>
+              <div style={
+                {textAlign: "center"}
+              }>
+                <video controls
+                  style={
+                    {width: '50%'}
+                  }
+                  src={
+                    this.props.state.videoURI
+                }></video>
+
+              </div>
+            </Col>
+
+
+            {/* Next steps... */} 
+            
+            </Space>
         </Row>
       </>
     );
