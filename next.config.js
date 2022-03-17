3136f21930bb226b2cd6a45550cd902df6b8360c
Nico Bilinkis <nico@bilinkis.com>
2022-03-17 17:58:52 -0300

    testing eslint config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 })({ target: "serverless" });
+
