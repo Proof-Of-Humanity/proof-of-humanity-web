6e264a8fffaa869b11c3441b2632b9a71bccc0cf
Nico Bilinkis <nico@bilinkis.com>
2022-04-19 10:59:05 -0300

    fix: added head and checks for loading

--- _pages/profile/submit/index.js
+++ _pages/profile/submit/index.js
@@ -75,7 +75,7 @@ export default function ProfileNew() {
     if (submission?.registered && canReapply === false) {
       message.error("You can't reapply yet", 5);
       router.push({ pathname: "/profile/[id]", query: { id: account } });
-    } else if (submission === null) setLoading(false);
+    } else if (submission === null || !account) setLoading(false);
   }, [submission, canReapply, router, account]);
 
   // console.log(props?.contract)
@@ -98,7 +98,12 @@ export default function ProfileNew() {
   // console.log('profile new', account, props);
 
   // console.log('isLoading', isLoading);
-  if (isLoading) return <LoadingOutlined style={{ fontSize: 60 }} spin />;
+  if (isLoading)
+    return (
+      <Row justify="center">
+        <LoadingOutlined style={{ fontSize: 60 }} spin />
+      </Row>
+    );
 
   if (account)
     return (
@@ -124,40 +129,49 @@ export default function ProfileNew() {
     );
 
   return (
-    <Row justify="center">
-      <Col
-        className="submit-profile-card"
-        style={{ textAlign: "center" }}
-        xs={{ span: 24 }}
-        xl={{ span: 12 }}
-      >
-        <Space direction="vertical" size={1}>
-          <Title level={2}>{t("submit_profile_title")}</Title>
-          <Title level={5}>{t("submit_profile_create_wallet")}</Title>
-          <Button
-            type="primary"
-            shape="round"
-            className="button-orange"
-            onClick={connect}
-          >
-            {t("submit_profile_connect_wallet")}
-          </Button>
-          <Paragraph>
-            <Trans
-              i18nKey="submit_profile_wallet_help"
-              t={t}
-              components={[
-                <Link
-                  key="1"
-                  href="https://ethereum.org/en/wallets/find-wallet/"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                />,
-              ]}
-            />
-          </Paragraph>
-        </Space>
-      </Col>
-    </Row>
+    <>
+      <Head>
+        <title>{t("submit_profile")} | Proof of Humanity</title>
+        <meta httpEquiv="cache-control" content="no-cache" />
+        <meta httpEquiv="expires" content={0} />
+        <meta httpEquiv="pragma" content="no-cache" />
+      </Head>
+
+      <Row justify="center">
+        <Col
+          className="submit-profile-card"
+          style={{ textAlign: "center" }}
+          xs={{ span: 24 }}
+          xl={{ span: 12 }}
+        >
+          <Space direction="vertical" size={1}>
+            <Title level={2}>{t("submit_profile_title")}</Title>
+            <Title level={5}>{t("submit_profile_create_wallet")}</Title>
+            <Button
+              type="primary"
+              shape="round"
+              className="button-orange"
+              onClick={connect}
+            >
+              {t("submit_profile_connect_wallet")}
+            </Button>
+            <Paragraph>
+              <Trans
+                i18nKey="submit_profile_wallet_help"
+                t={t}
+                components={[
+                  <Link
+                    key="1"
+                    href="https://ethereum.org/en/wallets/find-wallet/"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  />,
+                ]}
+              />
+            </Paragraph>
+          </Space>
+        </Col>
+      </Row>
+    </>
   );
 }
