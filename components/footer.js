a3a8594c238077c4ff12912249a8bc68001d4323
Santiago Siri <santisiri@gmail.com>
2023-01-10 19:56:02 +0100

    feat: better footer ready to show poh dapps

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 import { Col, Layout, Row } from "antd";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
 import Image from "./image";
 import Link from "./link";
@@ -8,7 +8,7 @@ import SocialIcons from "./social-icons";
 const { Footer } = Layout;
 
 export default function AppFooter() {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
 
   return (
     <Footer className="poh-footer">
@@ -16,31 +16,21 @@ export default function AppFooter() {
         <Col xs={12} lg={8}>
           <Row justify="start">
             <Link
-              sx={{ marginTop: 0 }}
-              className="poh-header-text"
-              variant="navigation"
-              newTab
-              href="https://proofofhumanity.eth.limo/"
-            >
-              {t("footer_learn_more")}
-            </Link>
-          </Row>
-        </Col>
-        <Col xs={0} lg={8}>
-          <Row justify="center">
-            <Link
               sx={{ alignItems: "center", display: "flex" }}
               newTab
               href="https://ubi.eth.limo"
             >
               <Image
-                sx={{ cursor: "pointer" }}
-                src="/images/ubi-logo.png"
-                width="72"
+                sx={{ cursor: "pointer", marginTop: "-2px" }}
+                src="/images/ubi-logo-color.png"
+                width="82"
                 height="auto"
               />
             </Link>
           </Row>
+        </Col>
+        <Col xs={0} lg={8}>
+          <Row justify="center" />
         </Col>
         <Col xs={12} lg={8}>
           <Row justify="end">
